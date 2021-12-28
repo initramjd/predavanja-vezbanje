291b64912645ef798e25626de4e66f0f18bf61e8
Initram89 <goran_sb@yahoo.com>
2021-12-28 19:46:18 +0100

    Array print helper method

---
+++ src/p28_12_2021/Zadatak1.java
@@ -0,0 +1,29 @@
+package p28_12_2021;
+
+public class Zadatak1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	String[] array = {"1", "12", "22", "21", "13", "17", "36"};
+	
+	printArray(array);
+	
+	
+	
+	}
+	
+	public static void printArray(String[] array) {
+		
+		for (int i = 0; i < array.length; i++) {
+			
+			System.out.print(array[i] + ", ");
+			
+		}
+		
+	}
+	
+	
+	
+
+}
